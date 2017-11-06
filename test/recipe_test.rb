d650b89e105eb34139c8b8e5d09b7af3c636d7ec
abarnes26 <abarnes26@yahoo.com>
2017-11-06 13:21:06 -0700

    building it2 test suite

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -9,12 +9,29 @@ class RecipeTest < Minitest::Test
     assert_equal "Cheese Pizza", r.name
   end
 
+  def test_ingredients_is_empty_to_start_with
+    r = Recipe.new("Cheese Pizza")
+
+    assert_equal ({}), r.ingredients
+  end
+
   def test_it_can_add_an_ingredient
     r = Recipe.new("Cheese Pizza")
     r.add_ingredient("Flour", 500) # 500 "UNIVERSAL UNITS"
     assert_equal ["Flour"], r.ingredient_types
     r.add_ingredient("Cheese", 1500)
     assert_equal ["Flour", "Cheese"], r.ingredient_types
+  end
+
+  def test_it_can_list_all_ingredient_names_and_amounts_via_hash
+    r = Recipe.new("Cheese Pizza")
+    r.add_ingredient("Flour", 500) # 500 "UNIVERSAL UNITS"
+    assert_equal ["Flour"], r.ingredient_types
+    r.add_ingredient("Cheese", 1500)
+    assert_equal ["Flour", "Cheese"], r.ingredient_types
+
+    assert_instance_of Hash, r.ingredients
+    assert_equal ({"Cheese" => 1500, "Flour" => 500}), r.ingredients
   end
 
   def test_it_tracks_amount_of_a_recipe_required
