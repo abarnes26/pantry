a8c20e9555ae841fe38909c5a0d0adc768baf353
abarnes26 <abarnes26@yahoo.com>
2017-11-06 13:05:29 -0700

    created stock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,13 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_instantiates
+    assert_instance_of Pantry, Pantry.new
+  end
+
+  def test_pantry_has_a_stock
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
 end
